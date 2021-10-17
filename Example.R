548fd9e10d43618388b24cf10158eb3ca211c715
SaiDhanyaa <89403336+SaiDhanyaa@users.noreply.github.com>
2021-10-18 01:07:10 +0530

    Add files via upload

---
+++ Example.R
@@ -0,0 +1,59 @@
+ 1+1
+2+90001245852 
+2+3*5/3-5+4*86/324+469*525
+x <- 4
+y<-5
+z<-10
+x*y*z
+X*Y*Z
+X  4
+1+1
+2+3*6
+exp(1)
+sqrt(10)
+pi
+2*pi*6378
+x<- -4
+y<- -63
+this.year <- 2021
+This.Year<- 2021
+
+A<- c(25,21,26,22)
+
+x<- c(1,25,7,9)
+y<- c(6,9,23,0) 
+x+y
+
+
+
+seq(4,20,by=8)
+
+
+
+x*y
+sqrt(x)
+ 
+y<9
+x[-1]<-5;x
+y[y<9]=2
+
+#data frame
+df <- data.frame(name=c("adhi","bhavya","cathy","dhanya"),age=c(20,21,23,25),salary=c(10000,20000,30000,50000))
+df
+
+df[c(4,1),3]
+
+exer=data.frame(x=c(1:3),y=c("a","b","c"))
+exer
+
+exer[1,1]
+exer[1, c(1,2)]
+exer[c(1,3), 2]
+exer[c(1,3),1]
+exer[c(1,3),c(1,2)]
+exer$x
+exer$y
+
+
+
+ 
